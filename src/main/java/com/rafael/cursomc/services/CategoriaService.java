90cfdb260ddd152dce31d1126c4350a3f1f427cf
RAfael Amaral <mukrafael@hotmail.com>
2019-04-01 12:34:37 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/rafael/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.rafael.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+	
+}
